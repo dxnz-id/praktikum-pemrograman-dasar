40ea40036d028aa878083c62d68189d3bcfb8e80
dxnz-id <mzidannakrusda@gmail.com>
2025-11-03 22:50:01 +0700

    refactor: enhance input validation and user feedback in tugas1 and tugas2 functions

--- Pertemuan 7/Belajar Ular/main.py
+++ Pertemuan 7/Belajar Ular/main.py
@@ -2,42 +2,57 @@
 ##################### Task 1 #####################
 ##################################################
 def tugas1() :
-  nilai1 =  int(input("Masukkan nilai 1: "))
+  while True :
+    try :
+      nilai1 =  int(input("Masukkan nilai 1: "))
 
-  if nilai1 > 100 or nilai1 < 0:
-    print("Nilai tidak valid")
-  elif nilai1 >= 90 :
-    print("Nilai A")
-  elif nilai1 >= 75 :
-    print("Nilai B")
-  elif nilai1 >= 60 :
-    print("Nilai BC")
-  elif nilai1 <= 50 :
-    print("Nilai C")
-    nilai2 = int(input("Masukkan nilai 2: "))
-    if nilai2 > 90 :
-      print("Pertahankan")
-    else :
-      print("Belajar lebih giat")
+      if nilai1 > 100 or nilai1 < 0:
+        print("Masukkan nilai antara 0-100")
+        continue
+      elif nilai1 >= 90 :
+        print("Nilai A")
+      elif nilai1 >= 75 :
+        print("Nilai B")
+      elif nilai1 >= 60 :
+        print("Nilai BC")
+      elif nilai1 <= 50 :
+        print("Nilai C")
+        nilai2 = int(input("Masukkan nilai 2: "))
+        if nilai2 > 90 :
+          print("Pertahankan")
+        else :
+          print("Belajar lebih giat")
+      break
+    except ValueError :
+      print("Input tidak valid, silakan masukkan nilai yang benar.")
 
+  
 ##################################################
 ##################### Task 2 #####################
 ##################################################
 def tugas2() :
-  print("LIBURANYUK | Menjual tiket murah dengan tujuan LabuanBajo")
-  print("[A] MaskapaiSupreme  : Rp. 1.000.000/ticket")
-  print("[B] MaskapaiKuda     : Rp. 800.000/ticket")
-  print("[C] MaskapaiMacan    : Rp. 500.000/ticket")
+  # Currency Format
+  def currency_format(amount):
+    return f"Rp. {amount:,}".replace(",", ".")
+  
+  print("LIBURANYUK | Menjual tiket murah dengan tujuan LabuanBajo\n[A] MaskapaiSupreme  : Rp. 1.000.000/ticket\n[B] MaskapaiKuda     : Rp. 800.000/ticket\n[C] MaskapaiMacan    : Rp. 500.000/ticket")
 
-  # Input
-  maskapai = input("Pilih maskapai [A/B/C]: ").lower()
-  if maskapai != "a" and maskapai != "b" and maskapai != "c":
-    print("Invalid maskapai")
-    exit()
-  ticket = int(input("jumlah ticket: "))
-  isOnline = input("Beli secara online [Y/n]: ").lower() or "y"
+  # Ticket
+  while True:
+    maskapai = input("Pilih maskapai [A/B/C]: ").lower()
+    if maskapai in ("a", "b", "c"):
+      break
+    print("Maskapai tidak valid, silakan masukkan nilai yang benar.")
+  while True :
+    try :
+      ticket = int(input("jumlah ticket: "))
+      if ticket <= 0 :
+        print("Masukkan nilai lebih besar dari 0")
+        continue
+      break
+    except :
+      print("Jumlah tiket tidak valid, silakan masukkan nilai yang benar.")
 
-  # Total
   total = 0
   if maskapai == "a" :
     total = (1000000 * ticket)
@@ -46,16 +61,19 @@ def tugas2() :
   elif maskapai == "c" :
     total = 500000 * ticket
 
-  print("="*100)
-
-  # Discount
-  if isOnline == "y" :
-    print("Selamat, anda mendapat diskon 10%")
-    print("Harga sebelum diskon :", f"{total:,}")
-    disc = int(total - total * .1)
-    print("Total pembelian anda :", f"{disc:,}")
-  elif isOnline == "n" :
-    print("Total pembelian anda :", f"{total:,}")
+  # Is Online / Discount
+  while True :
+    isOnline = input("Beli secara online [Y/n]: ").lower() or "y"
+    if isOnline == "y" :
+      disc = int(total - total * .1)
+      print("="*100, "\nSelamat, anda mendapat diskon 10%\nHarga sebelum diskon :", currency_format(total), "\nTotal pembelian anda :", currency_format(disc))
+      break
+    elif isOnline == "n" :
+      print("="*100, "\nTotal pembelian anda :", currency_format(total))
+      break
+    else :
+      print("Input tidak valid, silakan masukkan nilai yang benar.")
+      continue
 
   # Bonus
   if total > 10000000 :
@@ -69,8 +87,14 @@ def tugas2() :
 ##################################################
 ################# Task Selection #################
 ##################################################
-tugas = int(input("Mau jalanin tugas berapa: "))
-if tugas == 1:
-  tugas1()
-elif tugas == 2:
-  tugas2()
+def select_task() :
+  while True :
+    tugas = int(input("Mau jalanin tugas berapa: "))
+    if tugas == 1:
+      tugas1(); break
+    elif tugas == 2:
+      tugas2(); break
+    else :
+      print("Tugas tidak ditemukan"); continue
+
+select_task()
